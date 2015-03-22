c5e248bc60ada1d3e5db569291a46c79b6f20589
Diego De Souza <diegotrus7@gmail.com>
2015-03-21 23:27:28 -0400

    completed next birth of the week finder kata

--- next_birth-day_of_the_week_finder/next_birth-day_of_the_week_finder.rb
+++ next_birth-day_of_the_week_finder/next_birth-day_of_the_week_finder.rb
@@ -1,3 +1,10 @@
 def next_birthday_of_the_week(birthday)
-
+  count = 0
+  day = birthday.cwday
+  while true
+    birthday = birthday.next_year
+    count += 1
+    break if day == birthday.cwday
+  end
+  count
 end
