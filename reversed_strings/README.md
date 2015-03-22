65934d290d92ea60e2d4ddcc022af5c63b7d9dbb
Diego De Souza <diegotrus7@gmail.com>
2015-03-21 22:34:04 -0400

    completed reversed strings

---
+++ reversed_strings/README.md
@@ -0,0 +1,6 @@
+####Description
+Complete the solution so that it reverses the string value passed into it.
+
+####Example
+```
+solution('world') # returns 'dlrow'```
---
+++ reversed_strings/reversed_strings_test.rb
@@ -0,0 +1,4 @@
+Test.expect(solution('world') == 'dlrow')
+Test.expect(solution('hello') == 'olleh')
+Test.expect(solution('') == '')
+Test.expect(solution('h') == 'h')
