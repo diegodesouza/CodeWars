28506cefb74e33de097264682b518970f4b0a122
Diego De Souza <diegotrus7@gmail.com>
2015-03-25 20:41:12 -0400

    completed reverse list order kata

---
+++ reverse_list_order/reverse_list_test.rb
@@ -0,0 +1,4 @@
+Test.assert_equals(reverse_list([1,2,3,4]), [4,3,2,1])
+Test.assert_equals(reverse_list([3,1,5,4]), [4,5,1,3])
+Test.assert_equals(reverse_list([3,6,9,2]), [2,9,6,3])
+Test.assert_equals(reverse_list([1]), [1])
